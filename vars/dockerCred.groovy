06c7b04347810f78d05ba32c1d21697db36bae05
Yash Mane <yashmane9872@gmail.com>
2026-03-30 15:35:19 +0530

    Update dockerCred.groovy

--- vars/dockerCred.groovy
+++ vars/dockerCred.groovy
@@ -1,11 +1,11 @@
 def call(String CredID, String ImageID){
   withCredentials([usernamePassword(
-   credentialsId:"CredID",
+   credentialsId:"${CredID}",
    passwordVariable: "dockerHubPass",
    usernameVariable: "dockerHubUser"
    )]){
    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-   sh "docker image tag ${ImageID} ${env.dockerHubUser}/${ImageID}"
+   sh "docker image tag "${ImageID}" ${env.dockerHubUser}/"${ImageID}""
    sh "docker push ${env.dockerHubUser}/${ImageID}:${BUILD_NUMBER}"
    }
 }
