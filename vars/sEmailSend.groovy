759089c1a34b7e3d3c8f67e13f565da5ed8308c5
Yash Mane <yashmane9872@gmail.com>
2026-03-30 18:39:37 +0530

    Update and rename emailSend.groovy to sEmailSend.groovy

---
+++ vars/sEmailSend.groovy
@@ -0,0 +1,6 @@
+def call(){
+  emailext from: 'yashmane98721@gmail.com',
+                     to: 'awslearning781@gmail.com',
+                     subject: "Build Successful",
+                     body: "Good News Build Success. "
+}
