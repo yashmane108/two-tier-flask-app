bba6abe90e52ff3f45a43f73e985d708eee54d94
Yash Mane <yashmane9872@gmail.com>
2026-03-31 20:38:37 +0530

    Update email2.groovy

--- vars/email2.groovy
+++ vars/email2.groovy
@@ -1,14 +1,14 @@
 def call(String status, String toEmail) {
    if (status == "SUCCESS") {
        emailext(
-           from: 'yashmane98721@gmail.com',
+           from: 'yashmane9872@gmail.com',
            to: 'toEmail',
            subject: "Build Successful",
            body: "Good News Build Success."
        )
    } else if (status == "FAILURE") {
        emailext(
-           from: 'yashmane98721@gmail.com',
+           from: 'yashmane9872@gmail.com',
            to: 'toEmail',
            subject: "Build Failed",
            body: "Bad News Build Failed"
