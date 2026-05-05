1e079426e9e7c50451e3e037f970c0ca8d47ab0c
Yash Mane <yashmane9872@gmail.com>
2026-05-05 23:35:11 +0530

    Create sonarToken.groovy

---
+++ vars/sonarToken.groovy
@@ -0,0 +1,16 @@
+// This block fetches the secret 'sonar-token' and stores it in the variable SONAR_TOKEN
+def call(String SQtoken){
+  withCredentials([string(
+    credentialsId: "${SQtoken}", 
+    variable: 'SONAR_TOKEN'
+  )]) {
+  sh """
+  docker run --rm \
+  -e SONAR_HOST_URL="http://3.82.111.130:9000" \
+  -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=visitor-book" \
+  -e SONAR_TOKEN="${env.SONAR_TOKEN}" \
+  -v "\$(pwd):/usr/src" \ 
+  sonarsource/sonar-scanner-cli
+  """
+  }
+}
